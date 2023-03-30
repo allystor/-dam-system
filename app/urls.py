5ee2f5e0fd3527e23b6308e63fc4ce1cde791362
alvaro-dms <alvaro.veiga@dmslog.com>
2023-03-30 19:35:44 -0300

    feat:view in postman

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('bacias/', views.bacias_view, name='bacias'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Bacia
+
+def bacias_view(request):
+    bacias = Bacia.objects.all()
+    data = {
+        'bacias': []
+    }
+    for bacia in bacias:
+        data['bacias'].append({
+            'tipoBacia': bacia.tipoBacia.nome,
+            'profundidade': bacia.profundidade,
+            'capacidade': bacia.capacidade,
+            'largura': bacia.largura,
+            'comprimento': bacia.comprimento,
+            'engenheiro': bacia.engenheiro.nome,
+        })
+    return JsonResponse(data)
